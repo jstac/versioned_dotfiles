c224a0cff237c9d69c068e7cb6c60e93f47ee484
John Stachurski <john.stachurski@gmail.com>
2021-09-13 16:15:48 +1000

    added fish

---
+++ config.fish
@@ -0,0 +1,9 @@
+set fish_key_bindings fish_user_key_bindings
+
+# Define alias in config file
+alias vi="nvim"
+alias jp="jupyter notebook"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end

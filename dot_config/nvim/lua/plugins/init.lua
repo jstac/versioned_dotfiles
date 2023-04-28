c52718f528f52caea2f996f1feb82c8c6aececb0
John Stachurski <john.stachurski@gmail.com>
2023-04-28 18:50:51 +0900

    misc

---
+++ dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,48 @@
+require('lazy').setup({
+
+  {
+    'akinsho/toggleterm.nvim', 
+     version = "*", 
+     config = true
+  }, 
+
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  }, 
+
+  { -- favorite
+    'michalbachowski/vim-wombat256mod', 
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+    
+  { -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+
+  { -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+}, {})
+
+

fea5df5ba83ab6b60266d6ff8c90363eebd8e25e
John Stachurski <john.stachurski@gmail.com>
2025-10-22 05:53:16 +0900

    misc

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,17 +7,3 @@ require('core.plugins')
 require('core.plugin_config')
 require('core.colorscheme')
 
-
--- Add modules here
-local modules = {
-  "core.options",
-  "core.keymaps",
-  "core.colorscheme",
-}
-
--- Refresh module cache
-for k, v in pairs(modules) do
-  package.loaded[v] = nil
-  require(v)
-end
-
